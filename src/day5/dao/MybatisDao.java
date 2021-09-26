535722a88af9ec5ecd60d1b45e308f71f9fc1323
KimNaYeong <psi5059@naver.com>
2021-09-26 18:03:59 +0900

    day5 mybatisdao

---
+++ src/day5/dao/MybatisDao.java
@@ -0,0 +1,83 @@
+package day5.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import day3.dto.Customer;
+import day5.mybatis.SqlSessionBean;
+
+public class MybatisDao {
+
+	private static MybatisDao dao = new MybatisDao();
+	private MybatisDao() { }
+	public static MybatisDao getInstance() {
+		return dao;
+	}
+	
+	// 1.새로운 코딩 : SqlSession 객체를 생성해야합니다. -> 메소드에서. 
+	// sqlSessionFactory 객체 가져오기
+	SqlSessionFactory sqlFactory = SqlSessionBean.getSessionFactory();
+	
+	public List<Customer>selectAll(){
+		List<Customer> list = null;
+	// 2.SqlSession 객체 생성해서 sql 실행하기.
+		SqlSession mapper = sqlFactory.openSession();
+		list = mapper.selectList("selectAll");
+					//   ㄴ이 메소드는 리턴타입이 List<Object>
+		mapper.close();
+		return list;
+	}
+	
+	 public Customer select(int idx) {
+		 Customer cus = null;
+		 SqlSession mapper = sqlFactory.openSession();
+		 cus = mapper.selectOne("selectOne", idx);     //두번째 인자는 sql 실행에 필요한 파라미터입니다. 
+		 				// ㄴ 이 메소드는 리턴타입이 Object, select 조회결과가 1row일때
+		 mapper.close();
+		 return cus;
+	 }
+	
+	 public int insert(Customer cus) {
+		// SqlSession mapper = sqlFactory.openSession(true);    //auto commit을 true, 기본값 false
+		 SqlSession mapper = sqlFactory.openSession();
+		 int n = mapper.insert("insert", cus);     //auto commit?  n값 확인?
+		 mapper.commit();
+		 mapper.close();
+		 return cus.getIdx();
+	 }
+	 
+	 public int delete(int idx) {
+		 SqlSession mapper = sqlFactory.openSession();
+		 int n = mapper.delete("delete", idx);
+		 mapper.commit();
+		 mapper.close();
+		 return n;
+	 }
+	 
+	 public int update(Customer cus) {
+		 SqlSession mapper = sqlFactory.openSession();
+		 int n = mapper.update("update", cus);
+		 mapper.commit();
+		 mapper.close();
+		 return n;
+	 }
+	// 결론 insert, update, delete 메소드는 return값이 있습니다. -> 실행한 결과 행의 갯수
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
